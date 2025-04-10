f583c2dda50f5d6cd4c6326b4d5c46ddfb1a230c
nuv-test-semillero-1q2025 <javier.suarez@nuvu.cc>
2025-04-10 15:15:44 -0500

    Codigo Corregido nuv-test-github.py

--- lambda1/nuv-test-github.py
+++ lambda1/nuv-test-github.py
@@ -3,7 +3,7 @@ import json
 def lambda_handler(event, context):
     # TODO implement
     print('event')
-    print('Hola mundo!)
+    print('Hola mundo!')
           
     return {
         'statusCode': 200,
